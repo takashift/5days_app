16bb1a0c95de93376e8cb4557293b77c048c54c7
higechira <tsunashima@hpcs.cs.tsukuba.ac.jp>
2018-05-05 01:16:20 +0900

    2章まで終了。

---
+++ 5days_app/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5days_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int in1 = Int32.Parse(textBox1.Text);
+            int in2 = Int32.Parse(textBox2.Text);
+            //int ans = in1 + in2;
+            int ans = 0;
+
+            //if(label2.Text == "+")
+            //{
+            //    ans = in1 + in2;
+            //}
+            //else if (label2.Text == "-")
+            //{
+            //    ans = in1 - in2;
+            //}
+            //else if (label2.Text == "×")
+            //{
+            //    ans = in1 * in2;
+            //}
+            ////if (label2.Text == "÷")
+            //else
+            //{
+            //    ans = in1 / in2;
+            //}
+
+            switch (label2.Text)
+            {
+                case ("+"):
+                    ans = in1 + in2;
+                    break;
+                case ("-"):
+                    ans = in1 - in2;
+                    break;
+                case ("×"):
+                    ans = in1 * in2;
+                    break;
+                default:
+                    ans = in1 / in2;
+                    break;
+            }
+            label3.Text = ans.ToString();
+            //label1.Text = "こんにちは。";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "+";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "-";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "×";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "÷";
+        }
+    }
+}
---
+++ 5days_app/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("5days_app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("5days_app")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("dcae0e51-41a3-4c51-a272-b294c89603a2")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
